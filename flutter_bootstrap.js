5dd4b647479df89919b2a83d8ebd068b880a9169
wiza <bykovkirill98@gmailmail.com>
2026-06-11 00:15:54 +0400

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1951231129" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1869831480" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
